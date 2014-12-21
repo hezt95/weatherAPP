65dca92bf9f8fa3e1615295ad7e85c4ad49f529f
HeZitong <hezitong@HeZitongdeMacBook-Pro.local>
2014-12-21 16:29:55 +0800

    Initial Commit

---
+++ MyWeatherSecond/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyWeatherSecond
+//
+//  Created by HeZitong on 14/12/21.
+//  Copyright (c) 2014年 HeZitong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyWeatherSecond/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyWeatherSecond
+//
+//  Created by HeZitong on 14/12/21.
+//  Copyright (c) 2014年 HeZitong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
